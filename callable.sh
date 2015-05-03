81f10493575135e3bf91d7f2ffb2d302ac019ce4
Brandon Romano <BrandonRRomano@gmail.com>
2015-05-03 17:50:52 -0400

    Install single should now be working

--- callable.sh
+++ callable.sh
@@ -48,6 +48,9 @@ Self__callable_init() {
 # @param $2: `--global` to install globally
 ##################################################
 Self__callable_install() {
+    # Importing install helper
+    . "$Ash__active_module_directory/helper/install.sh"
+
     # If user is passing in URL
     if [[ -n "$1" ]]; then
         Self_install_url "$@"
@@ -57,65 +60,3 @@ Self__callable_install() {
         Self_install_modules_file
     fi
 }
-
-##################################################
-# This function will install a single module
-#
-# @param $1: The git HTTP or SSH URL to install
-# @param $2: `--global` to install globally
-##################################################
-Self_install_url() {
-    local path=""
-
-    # If global flag is passed
-    if [[ -n "$2" && "$2" == "--global" ]]; then
-        path="$Ash__source_directory/$Ash_global_modules_directory"
-
-    # If no global flag is passed
-    else
-        path="$Self_modules_directory_path"
-    fi
-
-    # Cloning
-    Logger__log "Installing $1"
-    local success=$(cd $path; git clone "$1" &> /dev/null; echo $?)
-
-    # Success
-    if [[ $success -eq 0 ]]; then
-        Self_validate_install "$1"
-
-    # Failure
-    else
-        Logger__error "Failed to clone $1"
-    fi
-}
-
-##################################################
-# Validates an install
-##################################################
-Self_validate_install() {
-    local repo_folder=""
-
-    # SSH
-    local ssh_regex="git@.*\:.*/(.*).git"
-    if [[ "$1" =~ "$ssh_regex" ]]; then
-        repo_folder=${BASH_REMATCH[1]}
-    fi
-
-    # HTTP(S)
-    local http_regex="https?\://.*/.*/(.*).git"
-    if [[ "$1" =~ $http_regex ]]; then
-        repo_folder="${BASH_REMATCH[1]}"
-    fi
-
-    echo $repo_folder
-}
-
-##################################################
-# This function will install all of the modules
-# from the ash_modules file locally
-##################################################
-Self_install_modules_file() {
-    Logger__log "Install Modules"
-}
-
---
+++ helper/install.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+Self_install_path="" # TBD at runtime
+
+##################################################
+# This function will install all of the modules
+# from the ash_modules file locally
+##################################################
+Self_install_modules_file() {
+    Logger__log "Install Modules"
+}
+
+##################################################
+# This function will install a single module
+#
+# @param $1: The git HTTP or SSH URL to install
+# @param $2: `--global` to install globally
+##################################################
+Self_install_url() {
+    # If global flag is passed
+    if [[ -n "$2" && "$2" == "--global" ]]; then
+        Self_install_path="$Ash__source_directory/$Ash_global_modules_directory"
+
+    # If no global flag is passed
+    else
+        Self_install_path="$Self_modules_directory_path"
+    fi
+
+    # Cloning
+    Logger__log "Installing $1"
+    local success=$(cd $Self_install_path; git clone "$1" &> /dev/null; echo $?)
+
+    # Success
+    if [[ $success -eq 0 ]]; then
+        Self_install_validate "$1"
+
+    # Failure
+    else
+        Logger__error "Failed to clone $1"
+    fi
+}
+
+##################################################
+# Validates an install
+##################################################
+Self_install_validate() {
+    local repo_folder=$(Self_install_get_folder "$1")
+    local repo_path="$Self_install_path/$repo_folder"
+    local repo_config="$repo_path/$Ash_config_filename"
+
+    # Invalid path
+    if [[ ! -d "$repo_path" ]]; then
+        Logger__error "Failed to clone $1"
+        return
+    fi
+
+    # No ash_config file
+    if [[ ! -f "$repo_config" ]]; then
+        rm -rf "$repo_path"
+        Logger__error "$1 is not a valid ash module"
+        return
+    fi
+
+    # Renaming git module to proper place
+    eval $(YamlParse__parse "$repo_config" "Self_install_validate_")
+    local new_path="$Self_install_path/$Self_install_validate_name"
+    if [[ -d $new_path ]]; then
+        Logger__error "Module '$Self_install_validate_name' is already installed"
+        rm -rf "$repo_path"
+    else
+        mv "$repo_path" "$new_path"
+    fi
+}
+
+##################################################
+# Gets the repo folder name from a git clone URL
+##################################################
+Self_install_get_folder() {
+    local repo_folder=""
+
+    # SSH
+    local ssh_regex="git@.*\:.*/(.*).git"
+    if [[ "$1" =~ $ssh_regex ]]; then
+        repo_folder=${BASH_REMATCH[1]}
+    fi
+
+    # HTTP(S)
+    local http_regex="https?\://.*/.*/(.*).git"
+    if [[ "$1" =~ $http_regex ]]; then
+        repo_folder="${BASH_REMATCH[1]}"
+    fi
+
+    echo $repo_folder
+}
